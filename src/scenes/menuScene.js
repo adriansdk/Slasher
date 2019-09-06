21028f84314bc2ecb1aaf17e6a8ecc61c98ec913
Adrian <adrianivsdk@gmail.com>
2019-09-06 19:41:29 -0400

    Finished adding animations and started developing button to start gameScene

--- src/scenes/menuScene.js
+++ src/scenes/menuScene.js
@@ -1,32 +1,60 @@
 import logo from "../assets/images/logo.png";
 import background from "../assets/images/background.png";
 
+var animation;
 
-class menuScene extends Phaser.Scene {
+class menuScene extends Phaser.Scene { 
     constructor() {
         super({ key: "menuScene" });
     }
 
     preload() {
         this.load.image('logo', logo);
-        this.load.spritesheet('background', background, { frameWidth: 1177, frameHeight: 784});
+        this.load.spritesheet('background', background, { frameWidth: 1177, frameHeight: 784 });
     }
 
     create() {
+        this.createAnims.call(this);
+
+        var anims = ["background", "background2"];
+
+        var onAnimationcomplete = function (anim) {
+            console.log("animationcomplete", anim.key);
+            var next = anims.shift();
+            if (next) {
+                this.play(next);
+            } else {
+                this.off("animationcomplete", onAnimationcomplete);
+            }
+        };
+
+        this.add.sprite(450, 300, "background").setScale(0.78)
+            .play(anims.shift())
+            .on("animationcomplete", onAnimationcomplete);
+        // animation = this.add.sprite(450, 300, 'background').play('background').setScale(0.78);
+        // animation.animationcomplete(chain('background2'))
+        this.add.image(450, 230, 'logo').setScale(0.2);   
+    }
+
+    createAnims() {
         this.anims.create({
             key: 'background',
             frames: this.anims.generateFrameNumbers('background', { start: 1, end: 46 }),
-            frameRate: 10,
+            frameRate: 12,
             repeat: 0
         });
-        this.add.sprite(450, 300, 'background').play('background').setScale(0.78);
-        this.add.image(450, 230, 'logo').setScale(0.2);
-
-
-        
+        this.anims.create({
+            key: 'background2',
+            frames: this.anims.generateFrameNumbers('background', { start: 43, end: 45 }),
+            frameRate: 7,
+            repeat: -1,
+            forward: false,
+        });
     }
+
+
     update() {
-        
+
     }
 }
 
